6c255e904f89d3b388cd08931e6991eaa99ea092
Syed Shahid Ashiq Ali <syedshahid1215@gmail.com>
2025-06-12 19:55:11 +0500

    Update page.tsx

--- src/app/reservation/create/page.tsx
+++ src/app/reservation/create/page.tsx
@@ -1,10 +1,13 @@
 import { ReservationForm } from "@/components/form/reservation-form";
+import { Suspense } from "react";
 
 export default function Page() {
   return (
     <main className="p-8">
       <h1 className="text-2xl font-bold mb-4">Create Reservation</h1>
-      <ReservationForm />
+      <Suspense fallback={<div>Loading...</div>}>
+        <ReservationForm />
+      </Suspense>
     </main>
   );
 }
